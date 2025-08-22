4d2e59b33746ebc7c80fa152940581780bd95d3b
Joao Pedro <joaopedrodiasde@gmail.com>
2025-08-22 17:31:15 -0300

    22/08/2025 2 horas aprendendo os conceito basicos do java (JVM, sintaxe base, variaveis, condicionais, vetores etc...)

---
+++ 1.0 Java primeiro programama/src/Main.java
@@ -0,0 +1,71 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo no Java");
+        ///  string
+//        String name = "Joao Pedro";
+//        //Number
+//        int dia = 4;
+//        int idade = 18;
+//
+//        if(idade <= 20){
+//            System.out.println("Voce tem menos de 20 anos, voce tem " + idade);
+//        }else{
+//            System.out.println("Voce tem mais de 20 anos voce tem " + idade);
+//        }
+//
+//        boolean eUmBoolean = true ;
+//           if(!eUmBoolean){
+//               System.out.println("Aqui e um Falo");
+//           }else{
+//               System.out.println("aqui e um verdadeiro");
+//           }
+//
+//        switch (dia) {
+//            case 1:
+//                System.out.println("Domingo");
+//                break;
+//            case 2:
+//                System.out.println("Segunda");
+//                break;
+//            case 3 :
+//                System.out.println("Terça feira ");
+//            default:
+//                System.out.println("Nao tem este dia senhor " + name);
+//        }
+
+//        int[] numeros = {0,1,2,3,4,5,6,7,8,9};
+//        String[] string = {"Joao", "Pedro", "Dias"};
+//        System.out.println(numeros[0]);
+//        System.out.println(string[1]);//
+
+//        for (int i = 0 ; i <= 10; i++){
+//            System.out.println("Contamos em " + i);
+//        };
+
+        int number = 0 ;
+
+//        while(number <= 10){
+//            System.out.println("estamos no while e o numero agora e " + number);
+//            number++;
+//        }
+
+//        do {
+//            System.out.println("Estou no Do while " + number);
+//            number++ ;
+//        }while(number <= 5);
+
+        int inteiro = 100 ;
+        double decimal = inteiro;
+        System.out.println("Valor interio para deciamal "+ decimal);
+
+        double decimalOvalor = 100.00;
+        int valorInteiro =(int)  decimalOvalor;
+        System.out.println("Valor decimal para inteiro "+ valorInteiro);
+
+    }
+}
+
